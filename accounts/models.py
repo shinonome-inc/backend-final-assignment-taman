bb8528244cc09a492efe0628cf068b181bd74b57
Tamane Noguchi <t.noguchi.23.4.890@4nonome.com>
2023-05-10 06:20:37 -0700

    Revert "Feature/signup"

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
 
 
-class User(AbstractUser):
-    email = models.EmailField()
+# class User(AbstractUser):
 
 
 # class FriendShip(models.Model):
