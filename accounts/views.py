41fb57dd8d3ad870f328ff313bf648c0d6425e94
tn <t.noguchi.23.4.890@4nonome.com>
2023-05-10 20:23:49 -0700

    correction of the second review

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,15 @@
-# from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .forms import SignupForm
+from .forms import LoginForm, SignupForm
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy("LOGIN_REDIRECT_URL")
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -18,3 +18,20 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class LoginView(LoginView):
+    form_class = LoginForm
+    success_url = reverse_lazy("LOGIN_REDIRECT_URL")
+
+    def form_valid_login(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        return response
+
+
+class LogoutView(LogoutView):
+    template_name = ""
